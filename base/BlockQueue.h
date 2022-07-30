3a9691116c97ac1d8f969b6a95b6cdfe793a9f56
jack <qxz1597@163.com>
2022-07-30 16:40:32 +0800

    阻塞队列

--- base/BlockQueue.h
+++ base/BlockQueue.h
@@ -1,6 +1,6 @@
 /**
  * @file BlockQueue.h
- * @author your name (you@domain.com)
+ * @author jack
  * @brief 阻塞队列
  * @version 0.1
  * @date 2022-07-30
@@ -30,14 +30,22 @@ private:
 public:
     void push(const T &item){
         std::unique_lock<std::mutex> lock(mtx_);
+        // 队列已满，push操作等待，while循环防止假通知
         while(queue_.size() >= capacity_){
-            condProducer_.wait(lock);  
+            condProducer_.wait(lock);
         }
-        queue_.push_back(item);
-        condConsumer_.notify_one();
+        queue_.push(item);
+        condConsumer_.notify_one(); //通知消费者消费
     }
+    /**
+     * @brief 取出队列操作 
+     * @param item 
+     * @return true 
+     * @return false 
+     */
     bool pop(T& item){
         std::unique_lock<std::mutex> lock(mtx_);
+        // 队列为空，pop操作等待，while循环防止假通知
         while(queue_.empty()){
             condConsumer_.wait(lock);
             if(isClosed_){
@@ -45,13 +53,20 @@ public:
             }
         }
         item = queue_.front();
-        queue_.pop_front();
-        condProducer_.notify_one();
+        queue_.pop();
+        condProducer_.notify_one(); // 通知生产者生产
         return true;
     }
+    /**
+     * @brief 带等待时间的pop操作，等待timeout秒后放弃pop
+     * @param item 
+     * @param timeout 
+     * @return true 
+     * @return false 
+     */
     bool pop(T& item, int timeout){
         std::unique_lock<std::mutex> lock(mtx_);
-        while(empty()){
+        while(queue_.empty()){
             if(condConsumer_.wait_for(lock, std::chrono::seconds(timeout)) == std::cv_status::timeout){
                 return false;
             };
@@ -64,31 +79,56 @@ public:
         condProducer_.notify_one();
         return true;
     };
+    /**
+     * @brief 清空队列
+     */
     void clear(){
         std::lock_guard<std::mutex> lock(mtx_);
         queue_.clear();
     }
-
+    /**
+     * @brief 判断队列是否为空
+     * @return true 
+     * @return false 
+     */
     bool empty() {
         std::lock_guard<std::mutex> lock(mtx_);
         return queue_.empty();
     }
+    /**
+     * @brief 判断队列是否满
+     * @return true 
+     * @return false 
+     */
     bool full() {
         std::lock_guard<std::mutex> lock(mtx_);
         return queue_.size() >= capacity_;
     }
+    /**
+     * @brief 返回队列的容量
+     * @return size_t 
+     */
     size_t capacity(){
         std::lock_guard<std::mutex> lock(mtx_);
         return capacity_;
     }
+    /**
+     * @brief 返回队列当前元素的个数
+     * @return size_t 
+     */
     size_t size(){
         std::lock_guard<std::mutex> lock(mtx_);
         return queue_.size();
     }
-
+    /**
+     * @brief 通知消费者消费
+     */
     void flush(){
         condConsumer_.notify_one();
     }
+    /**
+     * @brief 关闭队列，清除队列内容，并通知所有等待状态
+     */
     void close(){
         {
             std::lock_guard<std::mutex> lock(mtx_);
@@ -98,7 +138,10 @@ public:
         condConsumer_.notify_all();
         condProducer_.notify_all();
     }
-
+    /**
+     * @brief Construct a new Block Queue object
+     * @param capacity 默认容量
+     */
     explicit BlockQueue(size_t capacity = 1000)
         :capacity_(capacity),
          queue_()
@@ -106,6 +149,9 @@ public:
         assert(capacity > 0);
         isClosed_ = false;
     }
+    /**
+     * @brief Destroy the Block Queue object 
+     */
     ~BlockQueue(){
         close();
     }
